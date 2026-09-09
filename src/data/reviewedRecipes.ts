9b16bbdba484b70193c6305b2c6359731461dbaf
Avazel-Lab <garyclark.work@gmail.com>
2026-09-09 11:09:13 +0100

    Register reviewed burritos and Greek chicken

--- src/data/reviewedRecipes.ts
+++ src/data/reviewedRecipes.ts
@@ -3,10 +3,14 @@ import { beefMixedBeanChilli } from './reviewed/beefMixedBeanChilli'
 import { mildFamilyChickenCurry } from './reviewed/mildFamilyChickenCurry'
 import { lentilSpaghettiBolognese } from './reviewed/lentilSpaghettiBolognese'
 import { salmonPotatoesBroccoli } from './reviewed/salmonPotatoesBroccoli'
+import { chickenBeanBurritos } from './reviewed/chickenBeanBurritos'
+import { greekChickenPotatoes } from './reviewed/greekChickenPotatoes'
 
 export const reviewedRecipes: Record<string, Recipe> = {
   [beefMixedBeanChilli.id]: beefMixedBeanChilli,
   [mildFamilyChickenCurry.id]: mildFamilyChickenCurry,
   [lentilSpaghettiBolognese.id]: lentilSpaghettiBolognese,
-  [salmonPotatoesBroccoli.id]: salmonPotatoesBroccoli
+  [salmonPotatoesBroccoli.id]: salmonPotatoesBroccoli,
+  [chickenBeanBurritos.id]: chickenBeanBurritos,
+  [greekChickenPotatoes.id]: greekChickenPotatoes
 }
